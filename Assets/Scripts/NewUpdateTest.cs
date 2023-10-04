af7a3a215a9b7ed8fdcef2b1d2f837b91f2ca149
梦小天幼 <2090311442@qq.com>
2023-10-04 16:58:13 +0800

    温故而知新

---
+++ Assets/Scripts/NewUpdateTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewUpdateTest : MonoBehaviour
+{
+    public string abName;
+    public string resName;
+    public string resName2;
+    public Transform modelTran;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private AssetBundle ab;
+
+    public void Btn_UpdateRes()
+    {
+        if(!ab)
+        ab = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + abName);
+        foreach (Transform item in modelTran)
+        {
+            Destroy(item.gameObject);
+        }
+        try
+        {
+            Instantiate(ab.LoadAsset(resName, typeof(GameObject)) as GameObject, modelTran);
+        }
+        catch (System.Exception)
+        {
+            Instantiate(ab.LoadAsset(resName2, typeof(GameObject)) as GameObject, modelTran);
+            throw;
+        }
+        
+    }
+}
